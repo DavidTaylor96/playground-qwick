a59764ac70e9f00fbba2f38220deaad12e4a1646
David Taylor <david.taylor@zonal.co.uk>
2023-03-28 10:19:04 +0100

    feat: work out what section you are on

--- src/components/nav-bar/navBar.tsx
+++ src/components/nav-bar/navBar.tsx
@@ -1,37 +1,35 @@
 import { component$ } from '@builder.io/qwik';
 import type { IRoutes } from '~/routes';
-import './navBar.css'
+import './navBar.css';
 
 interface INavBar {
-	selectedSection: IRoutes; 
+  selectedSection: IRoutes;
 }
 
 export default component$((props: INavBar) => {
+  const routes: IRoutes[] = [
+    {
+      route: 'about',
+    },
+    {
+      route: 'work',
+    },
+    {
+      route: 'skills',
+    },
+  ];
 
-	const routes: IRoutes[] = [
-		{
-			route: 'about',
-		}, 
-			{
-				route: 'work',
-			}, 
-			{
-				route: 'skills',
-			}
-];
-
-
-	return (
-		<div class="nav-bar">
-			<h2>David Taylor</h2>
-				<div class="spacer" />
-					{routes.map((route, index) => (
-						<ul key={index} onClick$={() => (props.selectedSection.route = route.route)}>
-							<li class={route.route === props.selectedSection.route ? "active-route" : ""}>
-								<p>{route.route}</p>
-							</li>
-						</ul>
-						))}
-			</div>
-	);
+  return (
+    <div class="nav-bar">
+      <h2>David Taylor</h2>
+      <div class="spacer" />
+      {routes.map((route, index) => (
+        <ul key={index} onClick$={() => (props.selectedSection.route = route.route)}>
+          <li class={route.route === props.selectedSection.route ? 'active-route' : ''}>
+            <p>{route.route}</p>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { $, component$, useStore, useOn, useOnWindow, useOnDocument } from '@builder.io/qwik';
+import { $, component$, useStore, useOn, useOnDocument } from '@builder.io/qwik';
 import NavBar from '~/components/nav-bar/navBar';
 import About from '~/components/sections/about/about';
 import Skills from '~/components/sections/skills/skills';
 import Work from '~/components/sections/work/work';
 import './index.css';
 export interface IRoutes {
-	route: string;
+  route: string;
 }
 
 interface ISection {
@@ -15,91 +15,84 @@ interface ISection {
 }
 
 export default component$(() => {
+  const router = useStore<IRoutes>({
+    route: 'about',
+  });
 
-	const router = useStore<IRoutes>({
-		route: 'about',
-	});
-
-	useOn(
-		'click',
-		$((event) => {
-			if(router.route === 'about') {
-				event.preventDefault();
-					document.getElementById('about')!.scrollIntoView({ behavior: 'smooth' });
-			}
-			if(router.route === 'work') {
-				event.preventDefault();
-				document.getElementById('work')!.scrollIntoView({ behavior: 'smooth' });
-
-			}
-			if(router.route === 'skills') {
-				event.preventDefault();
-				document.getElementById('skills')!.scrollIntoView({ behavior: 'smooth' });
-			}
-		})
-	);
-
-
-	const sections: ISection[] = [
+  const sections = useStore<ISection[]>([
     {
       id: 'about',
       top: 0,
-      bottom: 0,
+      bottom: 100,
     },
     {
       id: 'work',
-      top: 0,
-      bottom: 0,
+      top: 100,
+      bottom: 200,
     },
     {
       id: 'skills',
-      top: 0,
-      bottom: 0,
+      top: 200,
+      bottom: 300,
     },
-  ];
+  ]);
 
-  useOnDocument('mount', $(() => {
-    // Calculate the top and bottom position of each section relative to the viewport
-    sections.forEach((section) => {
-      const element = document.getElementById(section.id);
-      if (element) {
-        section.top = element.getBoundingClientRect().top;
-        section.bottom = element.getBoundingClientRect().bottom;
+  useOn(
+    'click',
+    $((event) => {
+      if (router.route === 'about') {
+        event.preventDefault();
+        document.getElementById('about')!.scrollIntoView({ behavior: 'smooth' });
       }
-    });
-  }));
+      if (router.route === 'work') {
+        event.preventDefault();
+        document.getElementById('work')!.scrollIntoView({ behavior: 'smooth' });
+      }
+      if (router.route === 'skills') {
+        event.preventDefault();
+        document.getElementById('skills')!.scrollIntoView({ behavior: 'smooth' });
+      }
+    })
+  );
 
-	useOnDocument('scroll', $(() => {
-		// Get the current scroll position of the window
-		const scrollPosition = window.pageYOffset;
-	
-		// Find the section that is currently in view
-		let currentSection = sections[0];
-		sections.forEach((section) => {
-			if (section.top <= scrollPosition && section.bottom > scrollPosition) {
-				currentSection = section;
-			}
-		});
-	
-		// Update the router to reflect the current section
-		router.route = currentSection.id;
+  useOnDocument(
+    'scroll',
+    $(() => {
+      const scrollPosition = window.scrollY;
+      sections.forEach((section) => {
+        if (scrollPosition >= section.top && scrollPosition <= section.bottom) {
+          console.log(section.id);
+        }
+      });
+      const about = document.getElementById('about')!.getBoundingClientRect();
+      const work = document.getElementById('work')!.getBoundingClientRect();
+      const skills = document.getElementById('skills')!.getBoundingClientRect();
 
-		console.log(router.route, 'router.route');
-		console.log(currentSection.id, 'currentSection.id')
-	}));
+      if (scrollPosition >= about.top && scrollPosition <= about.bottom) {
+		console.log('about')
+        router.route = 'about';
+      }
+      if (scrollPosition >= work.top && scrollPosition <= work.bottom) {
+		console.log('work')
+        router.route = 'work';
+      }
+      if (scrollPosition >= skills.top && scrollPosition <= skills.bottom) {
+		console.log('skills')
+        router.route = 'skills';
+      }
+    })
+  );
 
-
-
-	return (
-		<>
-		<header>
-			<NavBar selectedSection={router} />
-		</header>
-		<main class="container">
-			<About id="about" />
-			<Work id="work" />
-			<Skills id="skills" />
-		</main>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <NavBar selectedSection={router} />
+      </header>
+      <main class="container">
+        <About id="about" />
+        <Work id="work" />
+        <Skills id="skills" />
+      </main>
+    </>
+  );
 });
