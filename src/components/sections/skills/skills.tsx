971ed7ce3cf576dfffd429f1ed9d0a75a0f4e8b7
David Taylor <david.taylor@zonal.co.uk>
2023-03-28 13:23:15 +0100

    feat: added left side content

--- src/components/sections/skills/skills.tsx
+++ src/components/sections/skills/skills.tsx
@@ -1,23 +1,41 @@
 import { component$ } from '@builder.io/qwik';
+import './skills.css';
 
 interface ISkills {
-	id: string;
+  id: string;
 }
 
 export default component$((props: ISkills) => {
-	return (
-		<div class="inner-container" id={props.id}>
-		<div class="panel-right">
-		<article class="right-inner-container">
-		<h1 class='right-panel-header'>Skills</h1>
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quis aspernatur fugit labore dolores, rerum nostrum officiis maiores pariatur tempora facilis, error quam officia. Sunt deleniti libero nostrum. A, vero.</p>
-		</article>
-		</div>
-		<div class="panel-left">
-			<p>
-				Hi, I'm <strong>John Doe</strong>. I'm a <strong>UI/UX Designer</strong> and <strong>Front-End Developer</strong> based in New York. I have rich experience in web site design and theming and building. I also have experience in WordPress and Shopify. I am good at creating custom WordPress themes and plugins.
-			</p>
-			</div>
-	</div>
-	);
+  const skills = [
+    'UX/UI Design',
+    'Front End Development',
+    'Mobile Development',
+    'Comunication',
+    'Problem Solving',
+    'Leadership',
+    'Collaboration',
+    'Creativity',
+    'Adaptability',
+  ];
+  return (
+    <div class="inner-container" id={props.id}>
+      <div class="panel-right">
+        <article class="right-inner-container">
+          <h1 class="right-panel-header">Skills</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit quis aspernatur fugit
+            labore dolores, rerum nostrum officiis maiores pariatur tempora facilis, error quam
+            officia. Sunt deleniti libero nostrum. A, vero.
+          </p>
+        </article>
+      </div>
+      <div class="panel-left">
+        {skills.map((skill, index) => (
+          <div key={index} class="skill-container">
+            <p>{skill}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 });
--- src/components/sections/work/work.tsx
+++ src/components/sections/work/work.tsx
@@ -1,24 +1,56 @@
 import { component$ } from '@builder.io/qwik';
-
+import KFC from '../../../assets/images/kfc.png';
+import Edinbrugh from '../../../assets/images/edinburghUniversity.png';
+import Medayo from '../../../assets/images/Medayo.png';
+import ScottishEnterprise from '../../../assets/images/scottishEnterPrise.png';
+import Waitrose from '../../../assets/images/waitrose.png';
+import SRUC from '../../../assets/images/sruc.png';
+import Zonal from '../../../assets/images/zonal.png';
+import './work.css';
 
 interface IWork {
-	id: string;
+  id: string;
 }
 
 export default component$((props: IWork) => {
-	return (
-		<div class="inner-container" id={props.id}>
-		<div class="panel-right">
-		<article class="right-inner-container">
-		<h1 class='right-panel-header'>Work</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptatibus totam magni perspiciatis cumque vel repellat deleniti ut, temporibus maxime deserunt. Ipsa nemo ipsum temporibus distinctio quas voluptas animi non.</p>
-		</article>
-		</div>
-		<div class="panel-left">
-			<p>
-				Hi, I'm <strong>John Doe</strong>. I'm a <strong>UI/UX Designer</strong> and <strong>Front-End Developer</strong> based in New York. I have rich experience in web site design and theming and building. I also have experience in WordPress and Shopify. I am good at creating custom WordPress themes and plugins.
-			</p>
-			</div>
-	</div>
-	);
+  return (
+    <div class="inner-container" id={props.id}>
+      <div class="panel-right">
+        <article class="right-inner-container">
+          <h1 class="right-panel-header">Work</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptatibus totam magni
+            perspiciatis cumque vel repellat deleniti ut, temporibus maxime deserunt. Ipsa nemo
+            ipsum temporibus distinctio quas voluptas animi non.
+          </p>
+        </article>
+      </div>
+      <div class="panel-left">
+        <div class="outer-image-container">
+          <div class="image-container">
+            <img src={KFC} alt="KFC" />
+          </div>
+          <div class="image-container">
+            <img src={Edinbrugh} alt="Edinbrugh" />
+          </div>
+          <div class="image-container">
+            <img src={Medayo} alt="Medayo" class="image-scale" />
+          </div>
+          <div class="image-container">
+            <img src={Waitrose} alt="Waitrose" class="image-scale" />
+          </div>
+          <div class="image-container">
+            <img src={ScottishEnterprise} alt="ScottishEnterprise" />
+          </div>
+
+          <div class="image-container">
+            <img src={SRUC} alt="SRUC" />
+          </div>
+          <div class="image-container">
+            <img src={Zonal} alt="Zonal" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 });
